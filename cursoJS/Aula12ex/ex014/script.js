a93a917be0a0bd5c4044fbe104eeaaf7302db635
joaosopran <159202685+joaosopran@users.noreply.github.com>
2024-04-04 20:01:46 -0300

    Exercicio que mudar a imagem de acordo com a hora que é

---
+++ cursoJS/Aula12ex/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar () {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas`
+
+if (hora >= 0 && hora < 12) {
+    // bom dia
+    img.src = 'manha grande.png'
+    document.body.style.background = 'orange' // para mudar a cor do fundo de acordo com o valor da variavel
+} else if (hora >=12 && hora < 18) {
+    // boa tarde
+    img.src = 'tarde grande.png'
+    document.body.style.background = 'yellowgreen'
+} else {
+    // boa noite
+    img.src = 'noite grande.png'
+    document.body.style.background = 'black'
+}
+}
