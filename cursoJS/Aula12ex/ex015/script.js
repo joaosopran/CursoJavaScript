64c17317415ad9f6fbbcfc07789d706bf644421a
joaosopran <159202685+joaosopran@users.noreply.github.com>
2024-04-04 20:27:47 -0300

    Codigo que troca imagem e calcula idade

---
+++ cursoJS/Aula12ex/ex015/script.js
@@ -0,0 +1,27 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[erro]')
+    } else {
+
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+        } else if (fsex[1].checked) {
+            gênero = 'mulher'
+        }
+
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+    }
+
+
+
+
+}
