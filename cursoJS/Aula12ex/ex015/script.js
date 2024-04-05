e5814359950d8d9c72cc60f2f724bc8c3eb6d712
joaosopran <159202685+joaosopran@users.noreply.github.com>
2024-04-05 08:56:08 -0300

    Finalizei o código para calculo de idade

--- cursoJS/Aula12ex/ex015/script.js
+++ cursoJS/Aula12ex/ex015/script.js
@@ -11,14 +11,37 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'jovem.jpg') // para carregar a imagem
+            } else if (idade < 21) {
+                // jovem
+            } else if (idade < 50) {
+                // adulto
+            } else {
+                // idoso
+            }
         } else if (fsex[1].checked) {
-            gênero = 'mulher'
+            gênero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                // criança
+            } else if (idade < 21) {
+                // jovem
+            } else if (idade < 50) {
+                // adulto
+            } else {
+                // idoso
+            }
         }
 
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.appendChild(img) // para mostrar a imagem abaixo
     }
 
 
