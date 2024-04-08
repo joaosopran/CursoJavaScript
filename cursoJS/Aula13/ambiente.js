f772d2ec5759fb38aa59a9ebed4a30a326864b26
joaosopran <159202685+joaosopran@users.noreply.github.com>
2024-04-08 08:47:39 -0300

    Repetições

---
+++ cursoJS/Aula13/ambiente.js
@@ -0,0 +1,20 @@
+
+/*
+var c = 1
+
+while (c <= 6) { // while é enquanto repetição no inicio
+    console.log('Tudo bem?')
+    c++
+}
+*/
+
+var c = 1
+
+do { // laço de repetição no final
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
