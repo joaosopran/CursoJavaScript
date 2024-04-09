e9bb0688e1c6c40ad3ab1d3da6d699c7cf1f5fa6
joaosopran <159202685+joaosopran@users.noreply.github.com>
2024-04-09 09:34:06 -0300

    Comentário

--- cursoJS/vetornatela.js
+++ cursoJS/vetornatela.js
@@ -9,6 +9,6 @@ for ( pos=0; pos < valores.length; pos++ ) {
 */
 
 
-for ( let pos in valores) {
+for ( let pos in valores) { // para aparecer a posição e valor de cada uma
     console.log(`A posicção ${pos} tem o valor ${valores[pos]}`)
 }
